bd27942f4c449f0be8b6e4305c0e3bbd621a2d43
praveen <77220724+praveenmca2@users.noreply.github.com>
2021-01-28 16:57:36 +0530

    Add files via upload

---
+++ praveen.js
@@ -0,0 +1,11 @@
+function add() {
+	var foo=document.getElementById('hi').innerHTML
+		foo++;
+		document.getElementById('hi').innerHTML=foo;
+}
+function bca() {
+	var foo=document.getElementById('oi').innerHTML
+		foo++;
+		document.getElementById('oi').innerHTML=foo;
+	
+}
